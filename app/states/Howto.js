94580151bebfdaa96f1d7fcd6b1812cb50a21cc2
Michal Lipski <kiwaczki@gmail.com>
2016-12-16 15:01:27 +0100

    how to play

--- app/states/Howto.js
+++ app/states/Howto.js
@@ -4,7 +4,26 @@
     MyGame.Howto.prototype = {
         create: function () {
 
-            var howTo =  this.game.add.image(50, 50, 'how-to' );
+            this.game.add.image(50, 50, 'howto1' );
+            this.game.add.image(50, 70, 'howto2' );
+            this.game.add.image(50, 100, 'howto3' );
+            this.game.add.image(50, 130, 'howto4' );
+            var alien = this.game.add.image(150, 120, 'alien' );
+            alien.scale.setTo(0.7, 0.7);
+
+            this.game.add.image(50, 160, 'howto5' );
+            var us = this.game.add.image(200, 150, 'bonus-small' );
+            us.scale.setTo(0.7, 0.7);
+            this.game.add.image(50, 190, 'howto6' );
+            var epic = this.game.add.image(150, 180, 'bonus-medium' );
+            epic.scale.setTo(0.7, 0.7);
+
+            this.game.add.image(50, 240, 'howto9' );
+            var shield = this.game.add.image(210, 225, 'star' );
+            shield.scale.setTo(0.7, 0.7);
+            this.game.add.image(50, 260, 'howto10' );
+
+
             this.game.input.onDown.add(this.startGame, this);
         },
         startGame: function () {
--- app/states/Preloader.js
+++ app/states/Preloader.js
@@ -4,22 +4,13 @@
     MyGame.Preloader.prototype = {
         preload: function () {
 
-            this.load.spritesheet('button-start', 'assets/button-start.png', 146, 51);
-            this.load.spritesheet('button-facebook', 'assets/button-start.png', 146, 51);
-            this.load.image('screen-howtoplay', 'assets/screen-howtoplay.png');
             this.load.image('space', 'assets/space.png');
-//            this.load.image('alien', 'assets/space-baddie.png');
             this.load.image('alien', 'assets/Bug50proc.png');
-//            this.load.image('bonus-small', 'assets/carrot.png');
             this.load.image('bonus-small', 'assets/Story50proc.png');
-//            this.load.image('star', 'assets/star.png');
             this.load.image('star', 'assets/shield-small.png');
-//            this.load.image('bonus-medium', 'assets/melon.png');
             this.load.image('bonus-medium', 'assets/Epic50proc.png');
-//            this.load.image('bonus-large', 'assets/pineapple.png');
             this.load.image('bonus-large', 'assets/epic.png');
             this.load.image('ship', 'assets/ship.png');
-//            this.load.image('ship', 'assets/shmup-ship.png');
             this.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
             this.load.image('bullet', 'assets/bullet.png');
             this.load.atlas('generic', 'assets/joystick/skins/generic-joystick.png', 'assets/joystick/skins/generic-joystick.json');
@@ -36,6 +27,15 @@
             this.load.image('text-share', 'assets/texts/share.png');
             this.load.image('text-sprint', 'assets/texts/sprint-completed.png');
             this.load.image('how-to', 'assets/texts/how-to.png');
+
+            this.load.image('howto1', 'assets/texts/howto/howto1.png');
+            this.load.image('howto2', 'assets/texts/howto/howto2.png');
+            this.load.image('howto3', 'assets/texts/howto/howto3.png');
+            this.load.image('howto4', 'assets/texts/howto/howto4.png');
+            this.load.image('howto5', 'assets/texts/howto/howto5.png');
+            this.load.image('howto6', 'assets/texts/howto/howto6a.png');
+            this.load.image('howto9', 'assets/texts/howto/howto9.png');
+            this.load.image('howto10', 'assets/texts/howto/howto11.png');
 
             this.load.image('pragmatists', 'assets/texts/pragma.png');
             this.load.image('presents', 'assets/texts/presents-2016.png');
